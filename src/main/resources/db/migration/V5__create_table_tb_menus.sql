95031a0ae081abceda4a641bccb51c16333ce3ec
cristian <cristian.c.lopez.m@gmail.com>
2024-04-17 08:04:12 -0500

    fix conflicts with flyway and the database

---
+++ src/main/resources/db/migration/V5__create_table_tb_menus.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tb_menus (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    price DECIMAL(10, 2) NOT NULL,
+    state BOOLEAN NOT NULL,
+    imageName VARCHAR(255),
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES tb_menu_categories(id)
+);
