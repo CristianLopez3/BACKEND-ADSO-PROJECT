c7daa200ea972c79bf137bae2b874c9b707e8a71
cristian <cristian.c.lopez.m@gmail.com>
2024-04-22 08:37:17 -0500

    add test data to categories and menu tables

---
+++ src/main/resources/db/migration/V8__insert_into_tb_menus.sql
@@ -0,0 +1,14 @@
+-- Menús para la categoría 1
+INSERT INTO tb_menus (title, description, price, state, image_name, category_id) VALUES
+('Menu 1', 'Descripción del menú 1', 10.99, true, 'f38aa322-9d5e-485f-848a-0f01cc222b90.jpg', 1),
+ ('Menu 2', 'Descripción del menú 2', 12.99, true, '1093007c-fdd9-4edf-bc04-707395e3bc57.jpeg', 1),
+('Menu 3', 'Descripción del menú 3', 8.99, true, '608681ec-cfb0-4574-93ae-4478b02b426b.jpg', 1),
+('Menu 4', 'Descripción del menú 4', 14.99, true, '9133da66-e64e-4a0f-8d08-467743bfacbf.jpg', 1),
+('Menu 5', 'Descripción del menú 5', 11.99, true, '98c891f8-6a38-4363-9a1c-304203614c40.jpg', 1),
+
+-- Menús para la categoría 2
+ ('Menu 6', 'Descripción del menú 6', 9.99, true, '42c948a1-0029-4c2c-bd54-023cf24fc146.jpg', 2),
+ ('Menu 7', 'Descripción del menú 7', 13.99, true, '78ae4481-1694-4bff-a6af-9f55e283dc16.jpg', 2),
+  ('Menu 8', 'Descripción del menú 8', 15.99, true, '22e4e44b-30ad-4988-b2f3-d4e079ea1441.jpg', 2),
+  ('Menu 9', 'Descripción del menú 9', 10.99, true, '8b8b6783-b431-4065-bc4d-097bc03efcf7.jpg', 2),
+  ('Menu 10', 'Descripción del menú 10', 16.99, true, '7d3bdf28-1976-4d78-8308-24f679e0e630.jpeg', 2);
