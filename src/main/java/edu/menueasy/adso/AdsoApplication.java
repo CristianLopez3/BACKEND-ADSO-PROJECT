d562cb497a1a38ac93b920ea3f1908a078c7fb1a
cristian <cristian.c.lopez.m@gmail.com>
2024-04-22 11:14:07 -0500

    cors config v1

--- src/main/java/edu/menueasy/adso/AdsoApplication.java
+++ src/main/java/edu/menueasy/adso/AdsoApplication.java
@@ -2,6 +2,9 @@ package edu.menueasy.adso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AdsoApplication {
@@ -10,4 +13,14 @@ public class AdsoApplication {
     SpringApplication.run(AdsoApplication.class, args);
   }
 
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:5174/").allowedMethods("*").allowedHeaders("*");
+      }
+    };
+
+  }
 }
