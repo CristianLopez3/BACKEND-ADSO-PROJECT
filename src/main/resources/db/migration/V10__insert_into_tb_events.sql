c56d5582e332f82d4e2d79ea5f63b6c1cbe0ea2c
CristianLMartinez <cristian.lm@globant.com>
2024-05-18 23:40:52 -0500

    update insert into tb events

--- src/main/resources/db/migration/V10__insert_into_tb_events.sql
+++ src/main/resources/db/migration/V10__insert_into_tb_events.sql
@@ -1,14 +1,7 @@
 INSERT INTO tb_events (title, description, discount, url)
 VALUES (
     'Celebrate Labor Day with us',
-    "Join us on for an unparalleled dining experience as we celebrate the
-        spirit of hard work and dedication. Enjoy a special day with your colleagues,
-        friends, and family in a cozy and relaxed atmosphere. From delicious dishes i
-        nspired by local cuisine to craft cocktails and exceptional service, at MenuEASY,
-        we take pride in offering a top-notch culinary experience for all our guests.
-        Additionally, to make this day even more special, we will be offering exclusive
-        promotions and surprises for our diners. Don\'t miss out! Book your table now and
-        join us to celebrate Labor Day in style at MenuEASY.",
+    "Join us on for an unparalleled dining experience as we celebrate the spirit of hard work and dedication. Enjoy a special day with your colleagues, friends, and family in a cozy and relaxed atmosphere. From delicious dishes inspired by local cuisine to craft cocktails and exceptional service, at MenuEASY, we take pride in offering a top-notch culinary experience for all our guests. Additionally, to make this day even more special, we will be offering exclusive promotions and surprises for our diners. Don\'t miss out! Book your table now and join us to celebrate Labor Day in style at MenuEASY.",
     30,
     '1093007c-fdd9-4edf-bc04-707395e3bc57.jpeg'
 );
