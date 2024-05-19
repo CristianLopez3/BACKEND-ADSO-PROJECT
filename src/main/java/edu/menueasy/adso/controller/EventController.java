9cad524f3b68dd61388957190aea63b5ed02031d
Ricardox29 <147215696+Ricardox29@users.noreply.github.com>
2024-05-18 22:02:06 -0500

    Creacion del los archivos, EventController y de todos los archivos del dominain del event

--- src/main/java/edu/menueasy/adso/controller/EventController.java
+++ src/main/java/edu/menueasy/adso/controller/EventController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/events")
+@RequestMapping("/api/v1/events")
 public class EventController {
 
     private final EventService eventService;
