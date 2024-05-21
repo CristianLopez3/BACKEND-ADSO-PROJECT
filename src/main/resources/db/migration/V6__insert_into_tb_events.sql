9ba24e1603ac88cfcb28b48ab413ff6d61dfaba1
CristianLopez3 <cristian.c.lopez.m@gmail.com>
2024-05-20 22:18:09 -0500

    fix flyway scheme, redux the quantity of files

--- src/main/resources/db/migration/V6__insert_into_tb_events.sql
+++ src/main/resources/db/migration/V6__insert_into_tb_events.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_events (title, description, discount, url)
+INSERT INTO tb_events (title, description, discount, image_url)
 VALUES (
     'Celebrate Labor Day with us',
     "Join us on for an unparalleled dining experience as we celebrate the spirit of hard work and dedication. Enjoy a special day with your colleagues, friends, and family in a cozy and relaxed atmosphere. From delicious dishes inspired by local cuisine to craft cocktails and exceptional service, at MenuEASY, we take pride in offering a top-notch culinary experience for all our guests. Additionally, to make this day even more special, we will be offering exclusive promotions and surprises for our diners. Don\'t miss out! Book your table now and join us to celebrate Labor Day in style at MenuEASY.",
