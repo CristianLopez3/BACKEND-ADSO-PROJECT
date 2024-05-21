3e0f75ec0a56e35d0e988585f710c6777581524d
CristianLopez3 <cristian.c.lopez.m@gmail.com>
2024-05-20 20:12:30 -0500

    Test bucket s3 in aws

--- src/main/java/edu/menueasy/adso/AdsoApplication.java
+++ src/main/java/edu/menueasy/adso/AdsoApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.StandardCharsets;
 
